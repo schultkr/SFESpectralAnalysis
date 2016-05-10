35ddcb32fcf89c4756442d76f43450814578e378
Christoph Schult <christoph.schult@cms.hu-berlin.de>
2016-05-10 18:08:27 +0200

    update read after reformatting r files

--- QID-2456-SFEFreqFilt/README.md
+++ QID-2456-SFEFreqFilt/README.md
@@ -25,8 +25,7 @@ Output: Two plots for the time and frequency domain of the Southern Oscillation 
 ```
 
 ![Picture1](FrequencyDomain.png)
-![Picture2](SignalSim.png)
-![Picture3](TimeDomain.png)
+![Picture2](TimeDomain.png)
 
 ```r
 # install and load packages
--- QID-2512-SFESpecSim/SFESpecSim.R
+++ QID-2512-SFESpecSim/SFESpecSim.R
@@ -10,7 +10,6 @@ par(mfrow = c(2, 1), mar = c(3, 3, 0, 2) + 0.1, mgp = c(1.5, 0.5, 0))
 n      =   250   # number of simulated values
 theta1 =   0.9   # parameter of MA(1)
 sigma  =   1     # variance of error term
-
 Fr     = 0:50 / 100
 
 # AR(1) processes
@@ -18,6 +17,7 @@ phi1 = 0.9   # parameter of AR(1)
 x    = arima.sim(n = n, model = list(ar = phi1), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
 l    = ARMAspec(model = list(ar = c(phi1)), plot = F) # compute theoretical spectral density
 
+# create plot for AR(1) process wiith phi = 0.9
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
 spec.pgram(x, log = "no", xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
@@ -27,6 +27,7 @@ phi1 = -0.9   # parameter of AR(1)
 x    = arima.sim(n = n, model = list(ar = phi1), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
 l    = ARMAspec(model = list(ar = c(phi1)), plot = F) # compute theoretical spectral density
 
+# create plot for AR(1) process wiith phi = -0.9
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
 spec.pgram(x, log = "no",xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
@@ -40,20 +41,22 @@ phi2 = -0.75 # AR(2) parameters
 x    = arima.sim(n = n,model = list(ar = c(phi1, phi2)), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
 l    = ARMAspec(model = list(ar = c(phi1, phi2)), plot = FALSE) # compute theoretical spectral density
 
+# create plot for AR(2) process wiith phi1 = 0.9 and phi2 = -0.75
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
 spec.pgram(x, log = "no",xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 lines(l$spec ~ l$freq, type = "l", lwd = 2, col = "darkgreen")
 
-
-phi1 = -0.1 ; phi2 = 0.5 # AR(2) parameters
+phi1 = -0.1
+phi2 = 0.5 # AR(2) parameters
 x    = arima.sim(n = n,model = list(ar = c(phi1, phi2)), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
 l    = ARMAspec(model = list(ar = c(phi1, phi2)), plot = FALSE) # compute theoretical spectral density
 
-plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd=2,cex.axis=0.8, cex.lab=0.9)
+# create plot for AR(2) process wiith phi1 = -0.1 and phi2 = 0.5
+plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
-spec.pgram(x, log = "no",xlab = "frequency", ylab = "spectrum", main="", sub="", lwd=2, cex.axis=0.8, cex.lab=0.9)
-lines(l$spec ~ l$freq, type="l", lwd=2, col="darkgreen")
+spec.pgram(x, log = "no",xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
+lines(l$spec ~ l$freq, type = "l", lwd = 2, col = "darkgreen")
 
 
 
@@ -62,27 +65,29 @@ theta1 = 0.9   # parameter of MA(1)
 x    = arima.sim(n = n,model = list(ma = theta1), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
 l    = ARMAspec(model = list(ma = c(theta1)), plot = FALSE) # compute theoretical spectral density
 
+# create plot for MA(1) process wiith theta1 = 0.9
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
 spec.pgram(x, log = "no", xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 lines(l$spec ~ l$freq, type = "l", lwd = 2, col = "darkgreen")
 
 theta1 = -0.9   # parameter of MA(1)
-x    = arima.sim(n = n, model = list(ma = theta1), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
-l    = ARMAspec(model = list(ma = c(theta1)), plot = FALSE) # compute theoretical spectral density
+x      = arima.sim(n = n, model = list(ma = theta1), rand.gen = function(n, ...) rnorm(n, 0, sigma)) # simulate process
+l      = ARMAspec(model = list(ma = c(theta1)), plot = FALSE) # compute theoretical spectral density
 
+# create plot for MA(1) process wiith theta1 = -0.9
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 abline(h = 0, lty = "dotted")
 spec.pgram(x, log = "no", xlab = "frequency", ylab = "spectrum", main = "", sub = "", lwd = 2, cex.axis = 0.8, cex.lab = 0.9)
 lines(l$spec ~ l$freq, type = "l", lwd = 2, col = "darkgreen")
 
 # SAR(1) process
-
 phi1   = 0.5
 PHI1   = 0.5    # Reset theta for Seasonal AR
 l      = ARMAspec(model=list(ar = phi1, seasonal = list(sar = PHI1, period = 12)), col = "magenta3", lwd = 3, cex.axis = 0.8, cex.lab = 0.9, plot = FALSE)
 x      = arima.sim(n = n,model = list(ar = phi1, seasonal = list(sar = PHI1, period = 12)), rand.gen=function(n, ...) rnorm(n, 0, sigma))
 
+# create plot for SAR(1) process wiith phi1 = 0.5
 plot(x, type = "l", xlab = "time", ylab = expression(X[t]), lwd = 2)
 abline(h = 0, lty = "dotted")
 
